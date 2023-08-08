d47dff9189667a3de1136e901c5d1429722d945b
RiaanJBredenhann <riaanbredenhann@gmail.com>
2023-08-08 10:39:58 +0200

    update

--- Classwork/chapter2/src/Products.js
+++ Classwork/chapter2/src/Products.js
@@ -1,11 +1,17 @@
+// imports the Component class from react library which we use to extend
 import React, { Component } from 'react';
 
+// we are able to use the render() function in the new Products class
+// since it extaends the Component class.
 class Products extends Component {
     render() {
         return (
             <div>
                 <h2>
                     Products
+                </h2>
+                <h2>
+                    Courses
                 </h2>
             </div>
         );
