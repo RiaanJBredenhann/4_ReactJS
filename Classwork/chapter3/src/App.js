59b5e36fc16a1e8cad53b794335cbb921f02ecce
RiaanJBredenhann <riaanbredenhann@gmail.com>
2023-08-08 11:15:58 +0200

    update

--- Classwork/chapter3/src/App.js
+++ Classwork/chapter3/src/App.js
@@ -4,11 +4,13 @@ import { Button } from "react-bootstrap";
 
 class App extends Component {
   render() {
+    const isValid = true;
+
     return (
       <div>
         <h1>My First React App!</h1>
         <Products />
-        <Button variant="primary" disabled>Default</Button>
+        <Button variant="primary" disabled={!isValid}>Default</Button>
       </div>
     );
   }
