5649908237efcf93d416bc96ee600ac640edf182
RiaanJBredenhann <riaanbredenhann@gmail.com>
2023-08-08 11:25:15 +0200

    update

--- Classwork/chapter3/src/App.js
+++ Classwork/chapter3/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Products from "./Products";
 import { Button } from "react-bootstrap";
+import Rating from "./Rating";
 
 class App extends Component {
   render() {
@@ -10,7 +11,12 @@ class App extends Component {
       <div>
         <h1>My First React App!</h1>
         <Products />
-        <Button variant="primary" disabled={!isValid}>Default</Button>
+        {/* <Button variant="primary" disabled={!isValid}>Default</Button> */}
+        <Rating rating="1" />
+        <Rating rating="2" />
+        <Rating rating="3" />
+        <Rating rating="4" />
+        <Rating rating="5" />
       </div>
     );
   }
