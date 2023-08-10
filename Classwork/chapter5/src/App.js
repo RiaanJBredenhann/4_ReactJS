c0725a0d840e51db7603d812fa07c5a1d0a502a5
RiaanJBredenhann <riaanbredenhann@gmail.com>
2023-08-10 11:32:51 +0200

    update

--- Classwork/chapter5/src/App.js
+++ Classwork/chapter5/src/App.js
@@ -1,37 +1,26 @@
-// import React, { Component } from "react";
-// import Products from "./Products";
-// import { Button } from "react-bootstrap";
-// import Rating from "./Rating";
-
-// class App extends Component {
-//   render() {
-//     const isValid = true;
-
-//     return (
-//       <div>
-//         <h1>My First React App!</h1>
-//         <Products />
-//         {/* <Button variant="primary" disabled={!isValid}>Default</Button> */}
-//         {/* <Rating rating="1" />
-//         <Rating rating="2" />
-//         <Rating rating="3" />
-//         <Rating rating="4" />
-//         <Rating rating="5" /> */}
-//       </div>
-//     );
-//   }
-// }
-// export default App;
-
+import './App.css';
+import React, { Component } from 'react';
+import JumboTronComponent from './JumboTronComponent';
+//import UserForm from './UserForm';
 import Products from './Products';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>My First React App!</h1>
-      <Products />
-    </div>
-  );
+class App extends Component {
+  formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        <JumboTronComponent>
+          This is a long sentence, and I want to insert content into the
+          jumbotron component from the outside.
+        </JumboTronComponent>
+        {/* <Products /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
