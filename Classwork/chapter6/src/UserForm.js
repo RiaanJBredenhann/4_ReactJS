ea1833f8a8dd91426bcc1d7278f7aa4b215ca9de
RiaanJBredenhann <riaanbredenhann@gmail.com>
2023-08-10 12:10:14 +0200

    final

--- Classwork/chapter6/src/UserForm.js
+++ Classwork/chapter6/src/UserForm.js
@@ -18,42 +18,42 @@ class UserForm extends Component {
                             errors.email = 'Required';
                         } else if (
                             !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-                ) {
-                    errors.email = 'Invalid email address';
-                } else if (values.email.length < 10) {
-                    errors.email = "Email address too short";
-                }
-                if (!values.password) {
-                    errors.password = "Required";
-                } else if (values.password.length < 8) {
-                    errors.password = "Password too short";
-                }
-                return errors;
-            }}
-            onSubmit={(values, { setSubmitting }) => {
-                setTimeout(() => {
-                    alert(JSON.stringify(values, null, 2));
-                    setSubmitting(false);
-                }, 400);
-            }}
-        >
-            {({ isSubmitting }) => (
-                <Form>
-                    <Field type="email" name="email" />
-                    <span style= {{ color:"red", fontWeight:"bold" }}>
-                        <ErrorMessage name="email" component="div" />
-                    </span>
-                    <Field type="password" name="password" />
-                    <span style= {{ color:"red", fontWeight:"bold" }}>
-                        <ErrorMessage name="password" component="div" />
-                    </span>
-                    <button type="submit" disabled={isSubmitting}>
-                        Submit
-                    </button>
-                </Form>
-             )}
-        </Formik>
-        </div >
+                        ) {
+                            errors.email = 'Invalid email address';
+                        } else if (values.email.length < 10) {
+                            errors.email = "Email address too short";
+                        }
+                        if (!values.password) {
+                            errors.password = "Required";
+                        } else if (values.password.length < 8) {
+                            errors.password = "Password too short";
+                        }
+                        return errors;
+                    }}
+                    onSubmit={(values, { setSubmitting }) => {
+                        setTimeout(() => {
+                            alert(JSON.stringify(values, null, 2));
+                            setSubmitting(false);
+                        }, 400);
+                    }}
+                >
+                    {({ isSubmitting }) => (
+                        <Form>
+                            <Field type="email" name="email" />
+                            <span style= {{ color:"red", fontWeight:"bold" }}>
+                                <ErrorMessage name="email" component="div" />
+                            </span>
+                            <Field type="password" name="password" />
+                            <span style= {{ color:"red", fontWeight:"bold" }}>
+                                <ErrorMessage name="password" component="div" />
+                            </span>
+                            <button type="submit" disabled={isSubmitting}>
+                                Submit
+                            </button>
+                        </Form>
+                    )}
+                </Formik>
+            </div >
         )
     }
 }
