0619c06a98e137a62dbcc547c8a39bff6dd0697b
RiaanJBredenhann <riaanbredenhann@gmail.com>
2023-08-11 11:12:14 +0200

    update

--- Classwork/chapter7/src/GitHub.js
+++ Classwork/chapter7/src/GitHub.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import ReactLoading from 'react-loading';
+import { Media, Form, FormGroup, FormControl, Button } from 'reactbootstrap';
 
 class GitHub extends Component {
 
     constructor() {
         super();
+        this.state = { 
+            data: [],
+            isLoading: true 
+        };
+    }
+
+    //-- when we need to call the server --//
+    componentDidMount() {
         this.getGitHubData("greg");
     }
 
@@ -14,13 +24,41 @@ class GitHub extends Component {
     //   direct as that is in the json structure of the GitHub response --//
     getGitHubData(_searchTerm) {
         axios.get("https://api.github.com/search/users?q=" + _searchTerm)
-        .then(res => console.log(res.data.items));
+        .then(res => {
+            this.setState({ 
+                isLoading: false,
+                data: res.data.items
+            })
+        });
     }
 
     render() {
+
+        const listUsers = this.state.data.map((user) => 
+            <Media key={user.id}>
+                <a href={user.html_url}>
+                    <img 
+                        width={64}
+                        height={64}
+                        className="mr-3"
+                        src={user.avatar_url}
+                        alt="Generic Placeholder"
+                    />
+                </a>
+                <Media.Body>
+                    <h5>Login: {user.login}</h5>
+                    <p>Id: {user.id}</p>
+                </Media.Body>
+            </Media>
+        );
+
         return (
             <div>
-
+                <h3>GitHub Users Results</h3>
+                { this.state.isLoading && 
+                    <ReactLoading type="spinningBubbles" color="#444" />
+                }
+                {listUsers}
             </div>
         )
     }
