25a6eba6f77b87718029afcbb8b48bd911043432
RiaanJBredenhann <riaanbredenhann@gmail.com>
2023-08-11 13:39:00 +0200

    update

--- Classwork/chapter8/src/App.js
+++ Classwork/chapter8/src/App.js
@@ -4,11 +4,13 @@ import JumboTronComponent from './JumboTronComponent';
 import UserForm from './UserForm';
 import Products from './Products';
 import GitHub from './GitHub';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Nav, Navbar } from 'react-bootstrap';
 
 class App extends Component {
-  formatName(user) {
-    return user.firstName + ' ' + user.lastName;
-  }
+  // formatName(user) {
+  //   return user.firstName + ' ' + user.lastName;
+  // }
 
   render() {
 
@@ -27,3 +29,51 @@ class App extends Component {
 }
 
 export default App;
+
+//-- Our header component contains BrowserRouter, Switch and Route imported from the 
+//   ‘react-router-dom library which provide the essential routing functionalities --//
+class Header extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#home">React Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/github">GitHub</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+          <Switch>
+            <Route path="/github" component={GitHub} />
+            <Route exact path="/" component={Home} />
+            <Route path="/*" component={NotFound} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        Home
+      </div>
+    );
+  }
+}
+
+class NotFound extends Component {
+  render() {
+    return (
+      <div>
+        Not Found
+      </div>
+    )
+  }
+}
