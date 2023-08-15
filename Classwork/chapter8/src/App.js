db6989cf3e208e6ba3432a09e16bcfbd0de5d89f
RiaanJBredenhann <riaanbredenhann@gmail.com>
2023-08-15 10:43:19 +0200

    final

--- Classwork/chapter8/src/App.js
+++ Classwork/chapter8/src/App.js
@@ -1,93 +1,60 @@
-import './App.css';
 import React, { Component } from 'react';
-import JumboTronComponent from './JumboTronComponent';
-import UserForm from './UserForm';
-import Products from './Products';
 import GitHub from './GitHub';
-import GitHubUser from './GitHubUser';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Nav, Navbar } from 'react-bootstrap';
+import GitHubUser from './GitHubUser';
 
-class App extends Component {
-  // formatName(user) {
-  //   return user.firstName + ' ' + user.lastName;
-  // }
 
-  render() {
-
+class App extends Component {       
+  render() {        
     return (
-      <div className="App">
-        <JumboTronComponent>
-          This is a long sentence, and I want to insert content into the
-          jumbotron component from the outside.
-        </JumboTronComponent>
-        <Products />
-        <UserForm />
-        <GitHub />
+      <div>
+        <Header />
       </div>
     );
   }
 }
-
 export default App;
 
-//-- Our header component contains BrowserRouter, Switch and Route imported from the 
-//   ‘react-router-dom library which provide the essential routing functionalities --//
 class Header extends Component {
-  render() {
+  render(){
     return (
-      <BrowserRouter>
-        <div>
-          <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="#home">React Bootstrap</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="mr-auto">
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/github">GitHub</Nav.Link>
-                <Nav.Link href="/users">Users</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-          <Switch>
-            <Route path="/github/user/:login/:id" component={GitHubUser} />
-            <Route path="/github" component={GitHub} />
-            <Route exact path="/" component={Home} />
-            <Route exact path="/users" component={Users} />
-            <Route path="/*" component={NotFound} />
-          </Switch>
-        </div>
-      </BrowserRouter>
-    );
+        <BrowserRouter>
+          <div>            
+            <Navbar bg="light" expand="lg">
+              <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                  <Nav.Link href="/">Home</Nav.Link>
+                  <Nav.Link href="/github">GitHub</Nav.Link>                  
+                </Nav>
+              </Navbar.Collapse>
+            </Navbar>
+            <Switch> 
+              <Route path="/github/user/:login/:id" component={GitHubUser} />                                                                
+              <Route path="/github" component={GitHub} />              
+              <Route exact path="/" component={Home} />             
+              <Route path="/*" component={NotFound} />                       
+            </Switch>   
+          </div>  
+        </BrowserRouter>              
+    )
   }
 }
 
 class Home extends Component {
-  render() {
+  render(){
     return (
       <div>
-        Home
+        Home        
       </div>
-    );
-  }
-}
-
-class Users extends Component {
-  render() {
-    return (
-      <div>
-        Users
-      </div>
-    );
+    )
   }
 }
 
 class NotFound extends Component {
-  render() {
-    return (
-      <div>
-        Not Found
-      </div>
-    )
+  render(){
+    return <div>Not Found</div>
   }
 }
