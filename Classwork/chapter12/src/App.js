39bdd5d8d6ae78d8607c052b52afc00ac30440c7
RiaanJBredenhann <riaanbredenhann@gmail.com>
2023-08-23 14:14:05 +0200

    update

--- Classwork/chapter12/src/App.js
+++ Classwork/chapter12/src/App.js
@@ -1,11 +1,12 @@
+//-- we import useState from the react library as we will be using it for hooks --//
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'react-bootstrap';
 
 const App = () => {
-  const postsUrl = "https://jsonplaceholder.typicode.com/posts"
-  const todosUrl = "https://jsonplaceholder.typicode.com/todos"
-  const [requested, setRequested] = useState(postsUrl)
+  const postsUrl = "https://jsonplaceholder.typicode.com/posts";
+  const todosUrl = "https://jsonplaceholder.typicode.com/todos";
+  const [requested, setRequested] = useState(postsUrl);
 
   return (
     <div>
@@ -18,7 +19,7 @@ const App = () => {
       <br />
       Requested: {requested}
     </div>
-  )
+  );
 }
 
 export default App;
