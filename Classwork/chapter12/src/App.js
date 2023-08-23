08f75ff043f0f350ec63cda46f692759548cf22d
RiaanJBredenhann <riaanbredenhann@gmail.com>
2023-08-23 14:13:02 +0200

    update

--- Classwork/chapter12/src/App.js
+++ Classwork/chapter12/src/App.js
@@ -1,10 +1,24 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'react-bootstrap';
+
 const App = () => {
-const postsUrl = "https://jsonplaceholder.typicode.com/posts"
-const todosUrl = "https://jsonplaceholder.typicode.com/todos"
-const [requested, setRequested] = useState(postsUrl)
-return(
-<div>
-<Button variant="link" onClick={() => setRequested(postsUrl)}>
+  const postsUrl = "https://jsonplaceholder.typicode.com/posts"
+  const todosUrl = "https://jsonplaceholder.typicode.com/todos"
+  const [requested, setRequested] = useState(postsUrl)
+
+  return (
+    <div>
+      <Button variant="link" onClick={() => setRequested(postsUrl)}>
+        Posts
+      </Button>
+      <Button variant="link" onClick={() => setRequested(todosUrl)}>
+        Todos
+      </Button>
+      <br />
+      Requested: {requested}
+    </div>
+  )
+}
+
+export default App;
